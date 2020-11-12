56b8cea13ddf2246024b5ad8ce399152f0cd9699
Ivan Vegner <ivanvegner@gmail.com>
2020-11-12 14:36:24 -0500

    Configurable 2 or 4 position params

--- network.py
+++ network.py
@@ -10,7 +10,7 @@ from bindsnet.encoding import PoissonEncoder
 
 class Prototype1(Network):
     def __init__(
-        self, n_input_channels, patch_shape, n_filters, n_l1_features, n_l2_features, dt=1
+        self, n_input_channels, patch_shape, n_filters, n_l1_features, n_l2_features, dt=1, use_4_position=True
     ):
         """
 
@@ -23,11 +23,16 @@ class Prototype1(Network):
         :param int `n_filters`: Number of filter neurons.
         :param int `n_l1_features`: Number of neurons in the L1 feature discovery layer.
         :param int `n_l2_features`: Number of neurons in the L2 feature discovery layer.
+
+        **Kwargs:**
+        :param int `dt`: Ms per tick, default is 1
+        :param bool `use_4_position`: Whether to use 4 position neurons instead of 2. Default is True.
+
         """
         super().__init__(dt=dt)
         input_size = np.prod([n_input_channels, *patch_shape])
         input_layer = Input(input_size, traces=True)
-        position_layer = Input(2, traces=True)
+        position_layer = Input(4 if use_4_position else 2, traces=True)
         filter_layer = LIFNodes(n_filters, traces=True)
         feature_l1 = LIFNodes(n_l1_features, traces=True)
         feature_l2 = LIFNodes(n_l2_features, traces=True)
@@ -53,6 +58,7 @@ class Prototype1(Network):
         self.monitor = NetworkMonitor(self)
         self.add_monitor(self.monitor, "monitor")
 
+        self.use_4_position = use_4_position
         self.n_input_channels = n_input_channels
         self.patch_shape = patch_shape
         self.dt = dt
@@ -61,7 +67,7 @@ class Prototype1(Network):
         # x: (batch, channels, height, width)
         encoded_x = self._make_patches(x, time_per_patch=time_per_patch)
         # x["encoded_patches"]: (batch_size, n_patches, time_per_patch, input_size)
-        # x["encoded_positions"]: (batch_size, n_patches, time_per_patch, 2)
+        # x["encoded_positions"]: (batch_size, n_patches, time_per_patch, 2|4)
         for patch_idx in range(encoded_x["encoded_patches"].size(1)):
             _raw_patch = encoded_x["patches"][:, patch_idx]
             _raw_position = encoded_x["positions"][:, patch_idx]
@@ -99,8 +105,10 @@ class Prototype1(Network):
             x.shape, patch_h, patch_w, padding=PADDING, stride=STRIDE
         )
         # normalize positions to [0,1]
-        # TODO: investigate using 4 positions instead of 2
         patch_positions /= torch.max(patch_positions, axis=0, keepdim=True)[0]  # (n_patches, 2)
+        if self.use_4_position:
+            inv_patch_positions = 1 - patch_positions
+            patch_positions = torch.cat([patch_positions, inv_patch_positions], dim=1) # (n_patches, 4)
         assert len(patch_positions) == n_patches
 
         # encode each patch separately
@@ -111,7 +119,7 @@ class Prototype1(Network):
 
         # encode positions
         encoded_positions = torch.stack([encoder(p * position_intensity) for p in patch_positions])
-        # encoded_positions: (n_patches, time, 2)
+        # encoded_positions: (n_patches, time, 2|4)
         # add batch dimension
         encoded_positions = encoded_positions.unsqueeze(0).expand(batch_size, -1, -1, -1)
 
